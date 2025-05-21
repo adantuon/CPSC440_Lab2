2bbbbcd5672f6c7b327a7789824b556ae253ed8c
adantuon <adantuon@mail.umw.edu>
2025-05-21 11:50:30 -0400

    Added framework for arrow key movement, added turtle primitive

--- CPSC440_Lab2.cpp
+++ CPSC440_Lab2.cpp
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Draws a turtle
+void makeTurtle(int x, int y);
+
 int main()
 {
     ALLEGRO_DISPLAY* display = NULL;
@@ -40,9 +43,13 @@ int main()
     al_register_event_source(EventQueue, al_get_display_event_source(display));
 
     bool exit = false;
+    int x = 400;
+    int y = 300;
 
     while (!exit) {
         al_clear_to_color(al_map_rgb(0, 0, 0));
+
+        makeTurtle(x, y);
 
         al_flip_display();
 
@@ -50,15 +57,70 @@ int main()
 
         if (Event.type == ALLEGRO_EVENT_KEY_DOWN) {
             switch (Event.keyboard.keycode) {
+                case ALLEGRO_KEY_UP:
+                    break;
+                case ALLEGRO_KEY_DOWN:
+                    break;
+                case ALLEGRO_KEY_LEFT:
+                    break;
+                case ALLEGRO_KEY_RIGHT:
+                    break;
                 case ALLEGRO_KEY_ESCAPE:
                     exit = true;
                     break;
             }
+        }
+
+        if (Event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+            exit = true;
         }
     }
 
     al_destroy_display(display);
 
     return 0;
+}
+
+void makeTurtle(int x, int y) {
+    //Color Palette
+    ALLEGRO_COLOR light_green = al_map_rgb(90, 145, 59);
+    ALLEGRO_COLOR green = al_map_rgb(43, 70, 28);
+    ALLEGRO_COLOR dark_green = al_map_rgb(25, 41, 16);
+    ALLEGRO_COLOR white = al_map_rgb(245, 245, 245);
+    ALLEGRO_COLOR black = al_map_rgb(10, 10, 10);
+
+    //Make Head
+    al_draw_filled_circle(x + 45, y, 18, light_green);
+
+    //Make Eyes
+    al_draw_filled_ellipse(x + 47, y + 12, 5, 3, white);
+    al_draw_filled_ellipse(x + 47, y - 12, 5, 3, white); //might need to change 3 to 4
+
+    al_draw_filled_ellipse(x + 47, y - 12, 4, 3, black);
+    al_draw_filled_ellipse(x + 47, y + 12, 4, 3, black);
+
+    //Make Tail
+    al_draw_filled_triangle(x - 56, y + 5, x - 56, y - 5, x - 71, y, light_green);
+
+    //Make Legs
+    al_draw_filled_circle(x - 50, y - 25, 12, light_green);
+    al_draw_filled_circle(x - 50, y + 25, 12, light_green);
+    al_draw_filled_circle(x + 25, y - 25, 12, light_green);
+    al_draw_filled_circle(x + 25, y + 25, 12, light_green);
+
+    //Make Shell
+    al_draw_filled_ellipse(x - 12, y, 50, 37, green);
+
+    //Make Shell Lines
+    al_draw_ellipse(x - 12, y, 50, 37, dark_green, 3);
+    al_draw_line(x - 12, y, x - 50, y - 25, dark_green, 3);
+    al_draw_line(x - 12, y, x - 50, y + 25, dark_green, 3);
+    al_draw_line(x - 12, y, x + 25, y - 25, dark_green, 3);
+    al_draw_line(x - 12, y, x + 25, y + 25, dark_green, 3);
+    al_draw_line(x - 62, y, x + 37, y, dark_green, 3);
+    al_draw_line(x - 12, y - 37, x - 12, y + 37, dark_green, 3);
+
+    al_draw_filled_ellipse(x - 12, y, 20, 15, green);
+    al_draw_ellipse(x - 12, y, 20, 15, dark_green, 3);
 }
 
